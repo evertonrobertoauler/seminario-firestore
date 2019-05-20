41df78964051eadf8a00a7fe71b49db9347afca4
Everton R. Auler <evertonrobertoauler@gmail.com>
2019-05-19 22:54:07 -0300

    Etapa 3 - Criando paginas e configurando a navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { IonicModule } from '@ionic/angular';
+import { LoginComponent } from './paginas/login/login.component';
+import { ListagemComponent } from './paginas/tarefas/listagem/listagem.component';
+import { FormularioComponent } from './paginas/tarefas/formulario/formulario.component';
+
+const ROTAS: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'tarefas', component: ListagemComponent },
+  { path: 'tarefas/adicionar', component: FormularioComponent },
+  { path: 'tarefas/:id', component: FormularioComponent },
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot()
-  ],
+  declarations: [AppComponent, LoginComponent, ListagemComponent, FormularioComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), RouterModule.forRoot(ROTAS)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
