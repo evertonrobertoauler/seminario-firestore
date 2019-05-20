42a15285abb2941cbd99508822eb89bab41da291
Everton R. Auler <evertonrobertoauler@gmail.com>
2019-05-19 22:55:36 -0300

    Etapa 4 - Configurar sdk firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './paginas/login/login.component';
@@ -18,7 +24,15 @@ const ROTAS: Routes = [
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, ListagemComponent, FormularioComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), RouterModule.forRoot(ROTAS)],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    RouterModule.forRoot(ROTAS),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule.enablePersistence({ synchronizeTabs: true }),
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
